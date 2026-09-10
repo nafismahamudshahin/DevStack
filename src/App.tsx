2028379be7dc5cc02ac18e962066d08906aaf63c
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-10 20:44:13 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
