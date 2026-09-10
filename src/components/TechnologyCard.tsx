1437b5013af4cef3ef296a7eea93bf6f7c55f561
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-11 00:28:14 +0600

    making technology card with grid layout

---
+++ src/components/TechnologyCard.tsx
@@ -0,0 +1,37 @@
+import type { ITechnology } from "../types/Types";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
+
+interface ITechnologyCard {
+    stack: ITechnology,
+}
+const TechnologyCard = ({ stack }: ITechnologyCard) => {
+    return (
+        <div className="bg-base-100 shadow-sm px-5 py-7 rounded-md space-y-6">
+            <div className="flex justify-between">
+                <div>
+                    <img className="w-15" src={stack.icon} alt={stack.name} />
+                </div>
+                <div>
+                    <span className="shadow rounded-sm font-semibold p-2">{stack.badge}</span>
+                </div>
+            </div>
+            <div className="h-38 space-y-4">
+                <div className="space-y-2">
+                    <h3 className="text-2xl font-semibold text-[#0A0F1D]">{stack.name}</h3>
+                    <p className="text-[#64748B]">{stack.description}</p>
+                </div>
+                <div className="flex justify-between text-[#64748B]">
+                    <span className="shadow-sm px-3 py-1">{stack.category}</span>
+                    <span className="shadow-sm px-3 py-1">{stack.difficulty}</span>
+                    <div className="flex gap-1 items-center">
+                        <FaRegStarHalfStroke className="text-yellow-400" />
+                        <span>{stack.rating}</span>
+                    </div>
+                </div>
+            </div>
+            <button className="btn w-full bg-[#0A0F1D] text-white">Add to Stack</button>
+        </div>
+    );
+};
+
+export default TechnologyCard;
--- src/components/TechnologyExplorer.tsx
+++ src/components/TechnologyExplorer.tsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import type { ITechnology } from "../types/Types";
+import TechnologyCard from "./TechnologyCard";
 interface TechnologyPromise {
     technologiesPromise: Promise<ITechnology[]>
 }
@@ -12,10 +13,15 @@ const TechnologyExplorer = ({ technologiesPromise }: TechnologyPromise) => {
             <div>
                 <h2 className="text-[#0F172A] mb-2 font-extrabold text-4xl">Explore the <span className="text-pink-500">Technologies</span></h2>
                 <p className="text-[#64748B] text-[16px]">Pick one technology per category to build your ideal stack.</p>
-                <div>
-                    {
-                        stacks.map(stack => <li key={stack.id}>{stack.name}</li>)
-                    }
+                <div className="grid grid-cols-12 gap-10 mt-8">
+                    <div className="col-span-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {
+                            stacks.map(stack => <TechnologyCard key={stack.id} stack={stack}></TechnologyCard>)
+                        }
+                    </div>
+                    <div className="col-span-3">
+
+                    </div>
                 </div>
             </div>
         </section>
