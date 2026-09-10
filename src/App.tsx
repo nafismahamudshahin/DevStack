3804e3f5e0deabd1024dfb66b17d1ce81118d5db
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-10 21:21:27 +0600

    install tailwindCss and daisyUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>hello</h1>
+      <h1>hello</h1>
     </>
   )
 }
